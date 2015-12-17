6eba0f75df0f5301bffe2c9743d5b83f064aeb74
Nikhilesh Prasannakumar <nikhilesh.prasannakumar@gmail.com>
2015-12-17 13:28:14 +0530

    initial commit of libraries

---
+++ StarShipPinMap_TIVA.h
@@ -0,0 +1,62 @@
+#include <Energia.h>
+
+#define LED_R PB_3
+#define LED_G PC_5
+#define LED_B PC_4
+
+#define LDR_NTC PE_5
+
+
+#define POT PB_5
+
+#define NAVIKEYS PE_1
+
+#define MIC_IN PE_2
+
+#define AUDIO_IN_L PD_2
+#define AUDIO_IN_R PD_3
+
+#define I2C_SCL PA_6
+#define I2C_SDA PA_7
+
+#define AUDIO_OUT_L PF_2
+#define AUDIO_OUT_R PF_3
+
+#define BUZZER PF_2
+
+#define SPI_MOSI PB_7
+#define SPI_MISO PB_6
+#define SPI_CLK PB_4
+#define SPI_CS_A PA_3
+#define SPI_CS_B PA_2
+
+#define DHT_IN PB_2
+
+#define LCD_D4 PE_4
+#define LCD_D5 PA_5
+#define LCD_D6 PE_3
+#define LCD_D7 PC_6
+#define LCD_EN PE_0
+#define LCD_RS PF_1
+
+#define KEYPAD_OUT1 PE_4
+#define KEYPAD_OUT2 PA_5
+#define KEYPAD_OUT3 PE_3
+#define KEYPAD_OUT4 PC_6
+#define KEYPAD_IN1 PC_7
+#define KEYPAD_IN2 PD_6
+#define KEYPAD_IN3 PD_7
+#define KEYPAD_IN4 PF_4
+
+#define GLCD_SEL1 SPI_CS_A
+#define GLCD_SEL2 SPI_CS_B
+#define GLCD_RESET PB_6
+#define GLCD_DC PA_4
+#define GLCD_SCK PB_4
+
+#define BT_KEY PF_0
+#define UART_TX PB_1
+#define UART_RX PB_0
+
+
+
---
+++ StarShipXP.h
@@ -0,0 +1,8 @@
+#include <DHT.h>
+#include <LiquidCrystal.h>
+#include <LM75.h>
+#include <PCF8583.h>
+#include <SD.h>
+#include <TFTv2.h>
+#include <StarShipPinMap_TIVA.h>
+#include <pitches.h>
